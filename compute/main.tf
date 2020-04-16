e597d246c243568a688751de99463d82cf6ab201
amit <amit.roy2007@gmail.com>
2020-04-16 20:59:27 +0200

    Update main.tf

--- compute/main.tf
+++ compute/main.tf
@@ -3,6 +3,7 @@ resource "google_compute_instance" "default" {
   count=3
   name         = "controller-${count.index}"
   machine_type = "n1-standard-1"
+  can_ip_forward   = false
   zone         = "us-west1-c"
 
   tags = ["kubernetes-the-hard-way","controller"]
@@ -10,26 +11,53 @@ resource "google_compute_instance" "default" {
   boot_disk {
     initialize_params {
       image = "centos-cloud/centos-7"
+      size=200
     }
   }
 
-  // Local SSD disk
-  scratch_disk {
-    interface = "SCSI"
-  }
 
   network_interface {
     count=3
     subnetwork="kubernetes"
-    network_ip=private-network-ip 10.240.0.1${count.index}
+    network_ip=10.240.0.1${count.index}
     network_tier="STANDARD"
 
-    access_config {
-      // Ephemeral IP
-    }
   }
   
   service_account {
     scopes = ["compute-rw","storage-ro","service-management","service-control","logging-write","monitoring"]
   }
 }
+
+
+
+resource "google_compute_instance" "default" {
+  count=3
+  name         = "worker-${count.index}"
+  machine_type = "n1-standard-1"
+  can_ip_forward   = false
+  zone         = "us-west1-c"
+
+  tags = ["kubernetes-the-hard-way","controller"]
+
+  boot_disk {
+    initialize_params {
+      image = "centos-cloud/centos-7"
+      size=200
+    }
+  }
+
+
+  network_interface {
+    count=3
+    subnetwork="kubernetes"
+    network_ip=10.240.0.2${count.index}
+    network_tier="STANDARD"
+
+  }
+  
+  service_account {
+    scopes = ["compute-rw","storage-ro","service-management","service-control","logging-write","monitoring"]
+  }
+}
+
