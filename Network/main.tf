9e047c78cd6db4e6440b3fec1f82debe572b7e5b
amit <amit.roy2007@gmail.com>
2020-04-16 23:27:17 +0200

    Update main.tf

--- Network/main.tf
+++ Network/main.tf
@@ -1,6 +1,12 @@
+resource "google_compute_subnetwork" "kubernetes" {
+  name          = "kubernetes"
+  ip_cidr_range = "10.2.0.0/16"
+  region        = "us-west1"
+  network       = google_compute_network.kubernetes-the-hard-way.self_link
+  
+}
 
-###Create VPC
-resource "google_compute_network" "default" {
-  name = "kubernetes-the-hard-way"
-  auto_create_subnetworks=false
+resource "google_compute_network" "kubernetes-the-hard-way" {
+  name                    = "kubernetes-the-hard-way"
+  auto_create_subnetworks = false
 }
