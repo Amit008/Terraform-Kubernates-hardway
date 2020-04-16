88116510fc16ddade27075d7d6a2e073f8a3d1b4
amit <amit.roy2007@gmail.com>
2020-04-16 21:54:24 +0200

    Update main.tf

--- firewall/main.tf
+++ firewall/main.tf
@@ -1,3 +1,8 @@
+gcloud compute firewall-rules create kubernetes-the-hard-way-allow-internal \
+  --allow tcp,udp,icmp \
+  --network kubernetes-the-hard-way \
+  source-ranges [10.240.0.0/24,10.200.0.0/16"]
+
 
 ###Create firewall rules
 resource "google_compute_firewall" "default" {
