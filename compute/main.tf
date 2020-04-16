6464432b041aae069ef05a45b859f10c663a028d
amit <amit.roy2007@gmail.com>
2020-04-16 20:07:05 +0200

    Update main.tf

--- compute/main.tf
+++ compute/main.tf
@@ -1,1 +1,32 @@
 
+resource "google_compute_instance" "default" {
+  count=3
+  name         = "controller-${count.index}"
+  machine_type = "n1-standard-1"
+  zone         = "us-west1-c"
+
+  tags = ["kubernetes-the-hard-way","controller"]
+
+  boot_disk {
+    initialize_params {
+      image = "centos-cloud/centos-7"
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "SCSI"
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+  
+  service_account {
+    scopes = ["compute-rw","storage-ro","service-management","service-control","logging-write","monitoring"]
+  }
+}
