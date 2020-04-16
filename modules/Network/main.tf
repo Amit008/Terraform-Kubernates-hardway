0c16ef5cb8694499b0015a6314bdedfb43e89c8d
amit <amit.roy2007@gmail.com>
2020-04-17 01:16:12 +0200

    Create main.tf

---
+++ modules/Network/main.tf
@@ -0,0 +1,19 @@
+resource "google_compute_subnetwork" "kubernetes" {
+  name          = "kubernetes"
+  ip_cidr_range = "10.2.0.0/16"
+  region        = "us-west1"
+  network       = google_compute_network.kubernetes-the-hard-way.self_link
+  
+}
+
+resource "google_compute_network" "kubernetes-the-hard-way" {
+  name                    = "kubernetes-the-hard-way"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_address" "kubernetes-the-hard-way" {
+  name = "kubernetes-the-hard-way"
+address_type="EXTERNAL"
+region="us-west1"
+network_tier="STANDARD"
+}
