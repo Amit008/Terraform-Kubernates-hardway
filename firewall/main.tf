0dafb8972142ef9251aa7be8fc7337f9db72fca2
amit <amit.roy2007@gmail.com>
2020-04-16 19:39:52 +0200

    Update main.tf

--- firewall/main.tf
+++ firewall/main.tf
@@ -1,1 +1,17 @@
 
+###Create firewall rules
+resource "google_compute_firewall" "default" {
+  name    = "kubernetes-the-hard-way-allow-external"
+  network = "kubernetes-the-hard-way"
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "6443"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
