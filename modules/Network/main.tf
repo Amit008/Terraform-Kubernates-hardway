14e331e93b1661a1ed6e95dccce983e790600a1a
amit <amit.roy2007@gmail.com>
2020-04-17 19:44:59 +0200

    Update main.tf

--- modules/Network/main.tf
+++ modules/Network/main.tf
@@ -1,7 +1,6 @@
 resource "google_compute_subnetwork" "kubernetes" {
   name          = "kubernetes"
-  address_type="INTERNAL"
-  ip_cidr_range = "10.2.0.0/16"
+  ip_cidr_range = "10.240.0.0/24"
   region        = "europe-west4"
   network       = google_compute_network.kubernetes-the-hard-way.self_link
   
