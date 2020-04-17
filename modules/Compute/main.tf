3b02707954653aae3160eeb858231ced30ab54f8
amit <amit.roy2007@gmail.com>
2020-04-18 00:41:24 +0200

    Update main.tf

--- modules/Compute/main.tf
+++ modules/Compute/main.tf
@@ -30,16 +30,16 @@ resource "google_compute_instance" "Compute-group-0" {
 wget -q --show-progress --https-only --timestamping https://github.com/etcd-io/etcd/releases/download/v3.4.0/etcd-v3.4.0-linux-amd64.tar.gz
 tar -xvf etcd-v3.4.0-linux-amd64.tar.gz
 sudo mv etcd-v3.4.0-linux-amd64/etcd* /usr/local/bin/
- mkdir -p /etc/etcd /var/lib/etcd
+ sudo mkdir -p /etc/etcd /var/lib/etcd
  cp ca.pem kubernetes-key.pem kubernetes.pem /etc/etcd/
 INTERNAL_IP=$(curl -s -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/ip)
 ETCD_NAME=$(hostname -s)
 wget -q --show-progress --https-only --timestamping https://github.com/Amit008/Terraform-Kubernates-hardway/blob/master/SystemServices/etcd.service
-mv etcd.service /etc/systemd/system/
+sudo mv etcd.service /etc/systemd/system/
 
-systemctl daemon-reload
-systemctl enable etcd
-systemctl start etcd
+sudo systemctl daemon-reload
+sudo systemctl enable etcd
+sudo systemctl start etcd
 
 wget -q --show-progress --https-only --timestamping \
   "https://storage.googleapis.com/kubernetes-release/release/v1.15.3/bin/linux/amd64/kube-apiserver" \
@@ -47,8 +47,8 @@ wget -q --show-progress --https-only --timestamping \
   "https://storage.googleapis.com/kubernetes-release/release/v1.15.3/bin/linux/amd64/kube-scheduler" \
   "https://storage.googleapis.com/kubernetes-release/release/v1.15.3/bin/linux/amd64/kubectl"
 
-chmod +x kube-apiserver kube-controller-manager kube-scheduler kubectl
- mv kube-apiserver kube-controller-manager kube-scheduler kubectl /usr/local/bin/
+sudo chmod +x kube-apiserver kube-controller-manager kube-scheduler kubectl
+sudo mv kube-apiserver kube-controller-manager kube-scheduler kubectl /usr/local/bin/
 
 mkdir -p /var/lib/kubernetes/
 
