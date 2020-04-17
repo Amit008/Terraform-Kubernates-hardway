25b37828dd116dc90f386af6c4fe70bb3cc29e1c
amit <amit.roy2007@gmail.com>
2020-04-17 23:57:39 +0200

    Update main.tf

--- modules/Compute/main.tf
+++ modules/Compute/main.tf
@@ -30,6 +30,16 @@ resource "google_compute_instance" "Compute-group-0" {
 wget -q --show-progress --https-only --timestamping https://github.com/etcd-io/etcd/releases/download/v3.4.0/etcd-v3.4.0-linux-amd64.tar.gz
 tar -xvf etcd-v3.4.0-linux-amd64.tar.gz
 sudo mv etcd-v3.4.0-linux-amd64/etcd* /usr/local/bin/
+ mkdir -p /etc/etcd /var/lib/etcd
+ cp ca.pem kubernetes-key.pem kubernetes.pem /etc/etcd/
+INTERNAL_IP=$(curl -s -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/ip)
+ETCD_NAME=$(hostname -s)
+wget -q --show-progress --https-only --timestamping https://github.com/Amit008/Terraform-Kubernates-hardway/blob/master/SystemServices/etcd.service
+mv etcd.service /etc/systemd/system/
+
+systemctl daemon-reload
+systemctl enable etcd
+systemctl start etcd
 EOH
   }
   
