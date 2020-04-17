4461c82a643f221c7c647341b248dfd9c5e28ad6
amit <amit.roy2007@gmail.com>
2020-04-17 17:58:51 +0200

    Update main.tf

--- kubernetes/main.tf
+++ kubernetes/main.tf
@@ -5,11 +5,12 @@ module "SetupNetwork" {
 
 module "SetupFirewall" {
   source = "../modules/Firewall"
+  network_name="${module.SetupNetwork.Network_Name}"
 
 }
 
 module "SetupComputeInstances" {
   source = "../modules/Compute"
-
+  subnetwork_name="${module.SetupNetwork.SubNetwork_Name}"
 }
 
