36fea17497c626aa899ef486ba4bcf7706704cc9
amit <amit.roy2007@gmail.com>
2020-04-17 18:27:48 +0200

    Update outputs.tf

--- modules/Network/outputs.tf
+++ modules/Network/outputs.tf
@@ -3,6 +3,6 @@ output "Network_Name" {
   value = "${google_compute_network.kubernetes-the-hard-way.name}"
 }
 
-output "SubnetNetwork_Name" {
+output "SubNetNetwork_Name" {
   value = "${google_compute_network.kubernetes.name}"
 }
