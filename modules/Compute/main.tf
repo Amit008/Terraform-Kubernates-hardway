ad55496b725c96f54ce10016ca70d60132eabaf4
amit <amit.roy2007@gmail.com>
2020-04-17 22:44:22 +0200

    Update main.tf

--- modules/Compute/main.tf
+++ modules/Compute/main.tf
@@ -73,6 +73,9 @@ resource "google_compute_instance" "Compute-group-1" {
 wget -q --show-progress --https-only --timestamping https://github.com/etcd-io/etcd/releases/download/v3.4.0/etcd-v3.4.0-linux-amd64.tar.gz
 tar -xvf etcd-v3.4.0-linux-amd64.tar.gz
 sudo mv etcd-v3.4.0-linux-amd64/etcd* /usr/local/bin/
+yum install -y update all
+yum install -y install socat conntrack ipset
+
 EOH
   }
   
