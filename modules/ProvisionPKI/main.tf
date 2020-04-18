2157157c27548c11c5375d14dc0a48e8fcd37318
amit <amit.roy2007@gmail.com>
2020-04-19 01:53:27 +0200

    Update main.tf

--- modules/ProvisionPKI/main.tf
+++ modules/ProvisionPKI/main.tf
@@ -1,28 +1,3 @@
-resource "null_resource" "prepare-download" {
-  # ...
-
-  provisioner "local-exec" {
-    command = "sudo mkdir -p ../../download"
-    }
-}
-
-
-resource "null_resource" "download-cfssl" {
-  # ...
-
-  provisioner "local-exec" {
-    command = "sudo curl -s -L -o ~/bin/cfssl https://pkg.cfssl.org/R1.2/cfssl_linux-amd64"
-  }
-}
-
-resource "null_resource" "download-cfssljson" {
-  # ...
-
-  provisioner "local-exec" {
-    command = "sudo curl -s -L -o ~/bin/cfssljson https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64"
-  }
-}
-
 
 resource "null_resource" "grant-permission-cfssl" {
   # ...
@@ -41,3 +16,19 @@ resource "null_resource" "setpath" {
   }
 }
 
+
+resource "null_resource" "download-cfssl" {
+  # ...
+
+  provisioner "local-exec" {
+    command = "sudo curl -s -L -o ~/bin/cfssl https://pkg.cfssl.org/R1.2/cfssl_linux-amd64"
+  }
+}
+
+resource "null_resource" "download-cfssljson" {
+  # ...
+
+  provisioner "local-exec" {
+    command = "sudo curl -s -L -o ~/bin/cfssljson https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64"
+  }
+}
