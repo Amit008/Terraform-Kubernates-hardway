67638ec191f2912df1fbbc7653f3e0d7be25d1c5
amit <amit.roy2007@gmail.com>
2020-04-18 22:34:59 +0200

    Update main.tf

--- modules/ProvisionPKI/main.tf
+++ modules/ProvisionPKI/main.tf
@@ -1,1 +1,28 @@
 
+resource "Install PKI cfssl" "Install-cfssl" {
+  # ...
+
+  provisioner "local-exec" {
+    command = "wget -q --show-progress --https-only --timestamping \
+  https://storage.googleapis.com/kubernetes-the-hard-way/cfssl/linux/cfssl -O /opt/download  \
+  https://storage.googleapis.com/kubernetes-the-hard-way/cfssl/linux/cfssljson -O /opt/download"
+  }
+}
+
+resource "grant permission" "Grant-permission-cfssl" {
+  # ...
+
+  provisioner "local-exec" {
+    command = "sudo chmod +x /opt/download/cfssl /opt/download/cfssljson"
+  }
+}
+
+
+resource "move-cfssl-executable" "cfssl-move-bin" {
+  # ...
+
+  provisioner "local-exec" {
+    command = "sudo chmod +x sudo mv /opt/download/cfssl /opt/download/cfssljson /usr/local/bin/"
+  }
+}
+
