45639a1a76714c1e2ab4bf105cc99980cd7fc0a0
amit <amit.roy2007@gmail.com>
2020-04-19 19:58:21 +0200

    Update main.tf

--- modules/ProvisionCA-TLS/main.tf
+++ modules/ProvisionCA-TLS/main.tf
@@ -52,14 +52,3 @@ resource "null_resource" "Generate-Service-account" {
 
  }
 }
-
-resource "null_resource" "Generate-API-Server-Certificate" {
-  # ...
- depends_on       = ["null_resource.Generate-CA"]
-  provisioner "local-exec" {
-  command = "cfssl gencert -ca=../../Certificate/ca.pem -ca-key=../../Certificate/ca-key.pem -config=../../JasonFiles/ca-config.json -hostname=10.32.0.1,10.240.0.10,10.240.0.11,10.240.0.12,${KUBERNETES_PUBLIC_ADDRESS},127.0.0.1,${KUBERNETES_HOSTNAMES} -profile=kubernetes ../../JasonFiles/kubernetes-csr.json | cfssljson -bare ../../Certificate/kubernetes"
- }
-}
-
-
-
