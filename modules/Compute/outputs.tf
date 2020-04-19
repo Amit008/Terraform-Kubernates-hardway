620c70b4dc3b780230d6d5d2b51f55b198215faa
amit <amit.roy2007@gmail.com>
2020-04-20 00:32:32 +0200

    Update outputs.tf

--- modules/Compute/outputs.tf
+++ modules/Compute/outputs.tf
@@ -1,8 +1,3 @@
-output "public_ip" {
-  value = "${module.SetupComputeInstances.kubernetes-the-hard-way.address}"
-}
-
-
 output "External_ips" {
   value = "${join(" ", google_compute_instance.compute.*.network_interface.0.access_config.0.assigned_nat_ip)}"
 }
