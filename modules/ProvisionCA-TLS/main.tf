b0721a21931be4ba5c7506affd737d2f9678389e
amit <amit.roy2007@gmail.com>
2020-04-20 01:10:05 +0200

    Update main.tf

--- modules/ProvisionCA-TLS/main.tf
+++ modules/ProvisionCA-TLS/main.tf
@@ -54,15 +54,14 @@ resource "null_resource" "Generate-Service-account" {
 }
 
 
-#resource "null_resource" "Generate-Kubelet-Client-Certificates" {
+resource "null_resource" "Generate-Kubelet-Client-Certificates" {
   # ...
-#  count=3
-# depends_on       = ["null_resource.Generate-CA"]
-#  provisioner "local-exec" {
-#  command = "cfssl gencert -ca=../../Certificate/ca.pem -ca-key=../../Certificate/ca-key.pem -config=../../JasonFiles/ca-config.json -hostname=worker-${count.index},${EXTERNAL_IP},${INTERNAL_IP} -profile=kubernetes worker-${count.index}-csr.json | cfssljson -bare ../../Certificate/worker-${count.index}"
-# }
-#}
-
+  count=3
+ depends_on       = ["null_resource.Generate-CA"]
+  provisioner "local-exec" {
+  command = "cfssl gencert -ca=../../Certificate/ca.pem -ca-key=../../Certificate/ca-key.pem -config=../../JasonFiles/ca-config.json -hostname=worker-${count.index},element([$var.external_ip],${count.index}),element([$var.internal_ip],${count.index}) -profile=kubernetes worker-${count.index}-csr.json | cfssljson -bare ../../Certificate/worker-${count.index}"
+ }
+}
 
 resource "null_resource" "API-Server-Certificate" {
   # ...
