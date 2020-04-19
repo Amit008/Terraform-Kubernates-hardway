9b78ad46598e1e7fa18a261c0b8fbd85dd98fc9d
amit <amit.roy2007@gmail.com>
2020-04-19 02:31:00 +0200

    Update main.tf

--- modules/ProvisionPKI/main.tf
+++ modules/ProvisionPKI/main.tf
@@ -2,6 +2,15 @@ resource "null_resource" "download-cfssl" {
   # ...
 
   provisioner "local-exec" {
+    command = "chmod 775 ../../Scripts/cfssl_Installation.sh"
+    interpreter = ["/bin/bash", "-c"]
+  }
+}
+
+resource "null_resource" "download-cfssl" {
+  # ...
+
+  provisioner "local-exec" {
     command = "../../Scripts/cfssl_Installation.sh"
     interpreter = ["/bin/bash", "-c"]
   }
