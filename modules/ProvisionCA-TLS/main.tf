e1fda42e8c445972432fa9cd8b1d5eb2de766254
amit <amit.roy2007@gmail.com>
2020-04-20 00:56:06 +0200

    Update main.tf

--- modules/ProvisionCA-TLS/main.tf
+++ modules/ProvisionCA-TLS/main.tf
@@ -62,3 +62,12 @@ resource "null_resource" "Generate-Service-account" {
 #  command = "cfssl gencert -ca=../../Certificate/ca.pem -ca-key=../../Certificate/ca-key.pem -config=../../JasonFiles/ca-config.json -hostname=worker-${count.index},${EXTERNAL_IP},${INTERNAL_IP} -profile=kubernetes worker-${count.index}-csr.json | cfssljson -bare ../../Certificate/worker-${count.index}"
 # }
 #}
+
+
+resource "null_resource" "API-Server-Certificate" {
+  # ...
+ depends_on       = ["null_resource.Generate-CA"]
+  provisioner "local-exec" {
+  command = "cfssl gencert -ca=../../Certificate/ca.pem -ca-key=../../Certificate/ca-key.pem -config=../../JasonFiles/ca-config.json -hostname=10.32.0.1,10.240.0.10,10.240.0.11,10.240.0.12,${var.KUBERNETES_PUBLIC_ADDRESS},127.0.0.1,${KUBERNETES_HOSTNAMES} -profile=kubernetes ../../JasonFiles/kubernetes-csr.json | cfssljson -bare ../../Certificate/kubernetes"
+}
+
