b05be0237aa82ae827345eed794535f1f909ce71
amit <amit.roy2007@gmail.com>
2020-04-19 02:37:15 +0200

    Update cfssl_Installation.sh

--- Scripts/cfssl_Installation.sh
+++ Scripts/cfssl_Installation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo curl -s -L -o /usr/local/bin/cfssl https://pkg.cfssl.org/R1.2/cfssl_linux-amd64"
-sudo curl -s -L -o /usr/local/bin/cfssljson https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64"
-sudo chmod 775 /usr/local/bin/cfssl*"
-export PATH=$PATH:/usr/local/bin"
+sudo curl -s -L -o /usr/local/bin/cfssl https://pkg.cfssl.org/R1.2/cfssl_linux-amd64
+sudo curl -s -L -o /usr/local/bin/cfssljson https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64
+sudo chmod 775 /usr/local/bin/cfssl*
+export PATH=$PATH:/usr/local/bin
    
