98be4ee6791272bf8155114f3e17368c739f0d42
amit <amit.roy2007@gmail.com>
2020-04-19 20:46:07 +0200

    Update variables.tf

--- modules/ProvisionCA-TLS/variables.tf
+++ modules/ProvisionCA-TLS/variables.tf
@@ -6,3 +6,5 @@ variable "external_ip" {
 variable "internal_ip" {
 
 }
+
+KUBERNETES_HOSTNAMES=kubernetes,kubernetes.default,kubernetes.default.svc,kubernetes.default.svc.cluster,kubernetes.svc.cluster.local
