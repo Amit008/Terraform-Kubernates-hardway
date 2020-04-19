d1ea068f29b1a83e5a17b26befe265a4599c0809
amit <amit.roy2007@gmail.com>
2020-04-19 06:02:51 +0200

    Update outputs.tf

--- modules/Compute/outputs.tf
+++ modules/Compute/outputs.tf
@@ -1,7 +1,7 @@
 output "External_IP" {
-  value = "${google_compute_subnetwork.kubernetes.name}"
+  value = "${google_compute_instances.default.network_interface.0.access_config.0.nat_ip}"
 }
 
 output "Internal_IP" {
-  value = "${google_compute_instance}"
+  value = "${google_compute_instances.default.default.network_interface.0.networkIP}"
 }
