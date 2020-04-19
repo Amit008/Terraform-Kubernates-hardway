37f1043a938961f3dab6ff2bc5b1993fc005ed7c
amit <amit.roy2007@gmail.com>
2020-04-19 19:11:54 +0200

    Update main.tf

--- modules/ProvisionPKI/main.tf
+++ modules/ProvisionPKI/main.tf
@@ -1,17 +1,20 @@
-resource "null_resource" "download-cfssl" {
-  # ...
-
-  provisioner "local-exec" {
-    command = "chmod 775 ../../Scripts/cfssl_Installation.sh"
-    interpreter = ["/bin/bash", "-c"]
+resource "null_resource" "Install-cfssl" {
+ provisioner "local-exec" {
+    command = "sudo curl -s -L -o /usr/local/bin/cfssl https://pkg.cfssl.org/R1.2/cfssl_linux-amd64"
   }
 }
 
-resource "null_resource" "download-cfssl" {
-  # ...
 
+resource "null_resource" "Install-cfssl" {
+ provisioner "local-exec" {
+    command = "sudo curl -s -L -o /usr/local/bin/cfssljson https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64"
+  }
+}
+
+
+resource "null_resource" "Grant-permission-script" {
+  depends_on = ["null_resource.Install-cfssl.id"] 
   provisioner "local-exec" {
-    command = "../../Scripts/cfssl_Installation.sh"
-    interpreter = ["/bin/bash", "-c"]
+    command = "sudo chmod 775 /usr/local/bin/cfssl*"
   }
 }
