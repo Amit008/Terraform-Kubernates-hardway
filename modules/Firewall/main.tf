6ee9ec8ab4c9ae6b5d510927685ae905c0d3524e
amit <amit.roy2007@gmail.com>
2020-04-20 00:21:41 +0200

    Update main.tf

--- modules/Firewall/main.tf
+++ modules/Firewall/main.tf
@@ -39,37 +39,37 @@ resource "google_compute_firewall" "kubernetes-the-hard-way-allow-external" {
 
 
 
-resource "google_compute_http_health_check" "kubernetes" {
-  name         = "authentication-health-check"
-  host         = "kubernetes.default.svc.cluster.local" 
-  request_path = "/healthz"  
-}
+#resource "google_compute_http_health_check" "kubernetes" {
+#  name         = "authentication-health-check"
+#  host         = "kubernetes.default.svc.cluster.local" 
+#  request_path = "/healthz"  
+#}
 
 
 
-resource "google_compute_firewall" "kube allow-health-check"
-{
-   name="kubernetes-the-hard-way-allow-health-check"
-   network = "${var.network_name}" 
-   allow {
-    protocol = "tcp"
-  }
+#resource "google_compute_firewall" "kube allow-health-check"
+#{
+#   name="kubernetes-the-hard-way-allow-health-check"
+#   network = "${var.network_name}" 
+#   allow {
+#    protocol = "tcp"
+#  }
 
-  source_ranges = ["209.85.152.0/22","209.85.204.0/22","35.191.0.0/16"]
-  }
+ # source_ranges = ["209.85.152.0/22","209.85.204.0/22","35.191.0.0/16"]
+ # }
 
 
-resource "google_compute_target_pool" "ktPool" {
-  name = "kubernetes-target-pool"
-  instances = [ "controller-0", "controller-1","controller-2"]
-  health_checks = [kubernetes]
-}
+#resource "google_compute_target_pool" "ktPool" {
+#  name = "kubernetes-target-pool"
+#  instances = [ "controller-0", "controller-1","controller-2"]
+#  health_checks = [kubernetes]
+#}
 
-resource "google_compute_forwarding_rule" "kubeforwarding-rule" {
-  name                  = "kubernetes-forwarding-rule"
-  region                = "europe-west4"
-  network               = "${var.network_name}"
-  ports                 = 6443
-  target                = google_compute_target_pool.ktPool.self_link
-  subnetwork            = "${google_compute_subnetwork.default.name}"
-}
+#resource "google_compute_forwarding_rule" "kubeforwarding-rule" {
+#  name                  = "kubernetes-forwarding-rule"
+#  region                = "europe-west4"
+#  network               = "${var.network_name}"
+#  ports                 = 6443
+#  target                = google_compute_target_pool.ktPool.self_link
+#subnetwork            = "${google_compute_subnetwork.default.name}"
+#}
