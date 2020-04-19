127b3ff174952eae5085aa1ebafdcc22a9c8e1d2
amit <amit.roy2007@gmail.com>
2020-04-19 04:43:33 +0200

    Update CreateCertificates.sh

--- Scripts/CreateCertificates.sh
+++ Scripts/CreateCertificates.sh
@@ -1,5 +1,5 @@
 cfssl gencert -initca ../../JasonFiles/ca-csr.json | cfssljson -bare ../../Certificate/ca
 cfssl gencert -ca=../../Certificate/ca.pem -ca-key=../../Certificate/ca-key.pem -config=../../JasonFiles/ca-config.json -profile=kubernetes ../../JasonFiles/admin-csr.json | cfssljson -bare ../../Certificate/admin
 cfssl gencert -ca=../../Certificate/ca.pem -ca-key=../../Certificate/ca-key.pem -config=../../JasonFiles/ca-config.json -profile=kubernetes ../../JasonFiles/kube-controller-manager-csr.json | cfssljson -bare ../../Certificate/kube-controller-manager
-cfssl gencert -ca=../../Certificate/ca.pem -ca-key=../../Certificate/ca-key.pem -config=../../JsonFiles/ca-config.json -profile=kubernetes ../../JsonFiles/service-account-csr.json | cfssljson -bare ../../Certificate/service-account
-cfssl gencert -ca=../../ca.pem -ca-key=ca-key.pem -config=../../JsonFiles/ca-config.json -profile=kubernetes ../../JsonFiles/kube-proxy-csr.json | cfssljson -bare ../../Certificate/kube-proxy
+cfssl gencert -ca=../../Certificate/ca.pem -ca-key=../../Certificate/ca-key.pem -config=../../JasonFiles/ca-config.json -profile=kubernetes ../../JasonFiles/service-account-csr.json | cfssljson -bare ../../Certificate/service-account
+cfssl gencert -ca=../../ca.pem -ca-key=ca-key.pem -config=../../JsonFiles/ca-config.json -profile=kubernetes ../../JasonFiles/kube-proxy-csr.json | cfssljson -bare ../../Certificate/kube-proxy
