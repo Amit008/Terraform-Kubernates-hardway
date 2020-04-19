f6f189fd224870f2a86de57773531e2ede3e9a0f
amit <amit.roy2007@gmail.com>
2020-04-19 18:54:32 +0200

    Update main.tf

--- modules/ProvisionCA-TLS/main.tf
+++ modules/ProvisionCA-TLS/main.tf
@@ -22,3 +22,12 @@ resource "null_resource" "Install-cfssl" {
     command = "sudo curl -s -L -o /usr/local/bin/cfssl https://pkg.cfssl.org/R1.2/cfssl_linux-amd64"
   }
 }
+
+
+resource "null_resource" "Grant-permission-script" {
+  # ...
+
+#  provisioner "local-exec" {
+#    command = "sudo chmod 775 /usr/local/bin/cfssl*"
+#  }
+#}
