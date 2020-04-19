3d6f19bab3d3ca3b1c9b313118d2302d435bbf55
amit <amit.roy2007@gmail.com>
2020-04-20 00:44:53 +0200

    Update outputs.tf

--- modules/Compute/outputs.tf
+++ modules/Compute/outputs.tf
@@ -1,5 +1,5 @@
 output "External_ips" {
-  value = "${join(" ",google_compute_instance.Compute-group-1.*.network_interface.0.access_config.0.assigned_nat_ip)}"
+  value = "${join(" ",google_compute_instance.Compute-group-1.*.network_interface.0.access_config.0.nat_ip)}"
 }
 
 output "Internal_ips" {
