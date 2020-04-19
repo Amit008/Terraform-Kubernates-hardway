e988678081d98d64f6126256e8662b545ca46d88
amit <amit.roy2007@gmail.com>
2020-04-19 04:19:40 +0200

    Update CreateCertificates.sh

--- Scripts/CreateCertificates.sh
+++ Scripts/CreateCertificates.sh
@@ -1,7 +1,8 @@
 cfssl gencert -initca ../../JasonFiles/ca-csr.json | cfssljson -bare ../../Certificate/ca
 cfssl gencert -ca=../../Certificate/ca.pem -ca-key=../../Certificate/ca-key.pem -config=../../JasonFiles/ca-config.json -profile=kubernetes ../../JasonFiles/admin-csr.json | cfssljson -bare ../../Certificate/admin
 cfssl gencert -ca=../../Certificate/ca.pem -ca-key=../../Certificate/ca-key.pem -config=../../JasonFiles/ca-config.json -profile=kubernetes ../../JasonFiles/kube-controller-manager-csr.json | cfssljson -bare ../../Certificate/kube-controller-manager
-
+cfssl gencert -ca=../../Certificate/ca.pem -ca-key=../../Certificate/ca-key.pem -config=../../JsonFiles/ca-config.json -profile=kubernetes ../../JsonFiles/service-account-csr.json | cfssljson -bare ../../Certificate/service-account
+cfssl gencert -ca=../../ca.pem -ca-key=ca-key.pem -config=../../JsonFiles/ca-config.json -profile=kubernetes ../../JsonFiles/kube-proxy-csr.json | cfssljson -bare ../../Certificate/kube-proxy
 
 for instance in worker-0 worker-1 worker-2; do
 cat > ${instance}-csr.json <<EOF
@@ -52,8 +53,4 @@ cfssl gencert \
   -hostname=10.32.0.1,10.240.0.10,10.240.0.11,10.240.0.12,${KUBERNETES_PUBLIC_ADDRESS},127.0.0.1,${KUBERNETES_HOSTNAMES} \
   -profile=kubernetes \
   ../../JsonFiles/kubernetes-csr.json | cfssljson -bare ../../Certificate/kubernetes
-  
-  
-  cfssl gencert -ca=../../Certificate/ca.pem -ca-key=../../Certificate/ca-key.pem -config=../../JsonFiles/ca-config.json -profile=kubernetes ../../JsonFiles/service-account-csr.json | cfssljson -bare ../../Certificate/service-account
-
-}
+ 
