2a2f09c3478105778c176c6151404d500d4cd52f
amit <amit.roy2007@gmail.com>
2020-04-19 19:10:16 +0200

    Update main.tf

--- modules/ProvisionCA-TLS/main.tf
+++ modules/ProvisionCA-TLS/main.tf
@@ -13,18 +13,3 @@
 #    command = "../../Scripts/CreateCertificates.sh"
 #  }
 #}
-
-
-resource "null_resource" "Install-cfssl" {
- provisioner "local-exec" {
-    command = "sudo curl -s -L -o /usr/local/bin/cfssl https://pkg.cfssl.org/R1.2/cfssl_linux-amd64"
-  }
-}
-
-
-resource "null_resource" "Grant-permission-script" {
-  depends_on = ["null_resource.Install-cfssl.id"] 
-  provisioner "local-exec" {
-    command = "sudo chmod 775 /usr/local/bin/cfssl*"
-  }
-}
