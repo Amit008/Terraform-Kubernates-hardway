745573dc6f4ad6757331da397099712a797dd42b
amit <amit.roy2007@gmail.com>
2020-04-20 09:36:37 +0200

    Update main.tf

--- modules/KubernetesConfiguration/main.tf
+++ modules/KubernetesConfiguration/main.tf
@@ -3,19 +3,18 @@ resource "null_resource" "Configure-Cluster" {
   # ...
   count =3
   provisioner "local-exec" {
-    command = "kubectl config set-cluster kubernetes-the-hard-way --certificate-authority=../../Certificate/ca.pem --embed-certs=true  --server=https://${KUBERNETES_PUBLIC_ADDRESS}:6443 --kubeconfig=../../KubernetesConfig/worker-${count.index}.kubeconfig
+    command = "kubectl config set-cluster kubernetes-the-hard-way --certificate-authority=../../Certificate/ca.pem --embed-certs=true  --server=https://${var.KUBERNETES_PUBLIC_ADDRESS}:6443 --kubeconfig=../../KubernetesConfig/worker-${count.index}.kubeconfig
 "
   }
   
  
 }
 
-
 resource "null_resource" "Set-Credentials-Cluster" {
   # ...
 count=3
   provisioner "local-exec" {
-    command = "kubectl config set-credentials system:node:worker-${count.index} --client-certificate=worker-${count.index}.pem --client-key=worker-${count.index}-key.pem --embed-certs=true  --kubeconfig=../../KubernetesConfig/worker-${count.index}.kubeconfig
+    command = "kubectl config set-credentials system:node:worker-${count.index} --client-certificate=../../Certificate/worker-${count.index}.pem --client-key=../../Certificate/worker-${count.index}-key.pem --embed-certs=true  --kubeconfig=../../KubernetesConfig/worker-${count.index}.kubeconfig
 "
   }
   
