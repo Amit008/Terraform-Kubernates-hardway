97220454153930bf967a861268d9e7d2b0c01fed
amit <amit.roy2007@gmail.com>
2020-04-20 09:41:42 +0200

    Update main.tf

--- modules/KubernetesConfiguration/main.tf
+++ modules/KubernetesConfiguration/main.tf
@@ -3,8 +3,7 @@ resource "null_resource" "Configure-Cluster" {
   # ...
   count =3
   provisioner "local-exec" {
-    command = "kubectl config set-cluster kubernetes-the-hard-way --certificate-authority=../../Certificate/ca.pem --embed-certs=true  --server=https://${var.KUBERNETES_PUBLIC_ADDRESS}:6443 --kubeconfig=../../KubernetesConfig/worker-${count.index}.kubeconfig
-"
+    command = "kubectl config set-cluster kubernetes-the-hard-way --certificate-authority=../../Certificate/ca.pem --embed-certs=true  --server=https://${var.KUBERNETES_PUBLIC_ADDRESS}:6443 --kubeconfig=../../KubernetesConfig/worker-${count.index}.kubeconfig"
   }
   
  
@@ -14,8 +13,7 @@ resource "null_resource" "Set-Credentials-Cluster" {
   # ...
 count=3
   provisioner "local-exec" {
-    command = "kubectl config set-credentials system:node:worker-${count.index} --client-certificate=../../Certificate/worker-${count.index}.pem --client-key=../../Certificate/worker-${count.index}-key.pem --embed-certs=true  --kubeconfig=../../KubernetesConfig/worker-${count.index}.kubeconfig
-"
+    command = "kubectl config set-credentials system:node:worker-${count.index} --client-certificate=../../Certificate/worker-${count.index}.pem --client-key=../../Certificate/worker-${count.index}-key.pem --embed-certs=true  --kubeconfig=../../KubernetesConfig/worker-${count.index}.kubeconfig"
   }
   
  
@@ -24,8 +22,8 @@ count=3
 resource "null_resource" "Set-Context" {
   # ...
 count=3
-  provisioner "local-exec" {
-    command = " kubectl config set-context default --cluster=kubernetes-the-hard-way  --user=system:node:worker-${count.index}  --kubeconfig=../../KubernetesConfig/worker-${count.index}.kubeconfig"
+provisioner "local-exec" {
+command = " kubectl config set-context default --cluster=kubernetes-the-hard-way  --user=system:node:worker-${count.index}  --kubeconfig=../../KubernetesConfig/worker-${count.index}.kubeconfig"
   }
   
  
@@ -35,7 +33,8 @@ resource "null_resource" "Set-User-Context" {
   # ...
 count=3
   provisioner "local-exec" {
-    command = " kubectl config use-context default --kubeconfig=../../KubernetesConfig/worker-${count.index}.kubeconfig"  }
+    command = " kubectl config use-context default --kubeconfig=../../KubernetesConfig/worker-${count.index}.kubeconfig" 
+    }
   
 }
 
