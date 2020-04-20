d5cb8db578a7028e2b9afef3707de732e21362a3
amit <amit.roy2007@gmail.com>
2020-04-20 09:32:38 +0200

    Update main.tf

--- modules/KubernetesConfiguration/main.tf
+++ modules/KubernetesConfiguration/main.tf
@@ -1,9 +1,9 @@
 
-resource "null_resource" "Generate-CA" {
+resource "null_resource" "Configure-Cluster" {
   # ...
-
+  count =3
   provisioner "local-exec" {
-    command = "kubectl config set-cluster kubernetes-the-hard-way --certificate-authority=ca.pem --embed-certs=true  --server=https://${KUBERNETES_PUBLIC_ADDRESS}:6443 --kubeconfig=${instance}.kubeconfig
+    command = "kubectl config set-cluster kubernetes-the-hard-way --certificate-authority=../../Certificate/ca.pem --embed-certs=true  --server=https://${KUBERNETES_PUBLIC_ADDRESS}:6443 --kubeconfig=../../KubernetesConfig/worker-${count.index}.kubeconfig
 "
   }
   
@@ -11,32 +11,32 @@ resource "null_resource" "Generate-CA" {
 }
 
 
-resource "null_resource" "Generate-CA" {
+resource "null_resource" "Set-Credentials-Cluster" {
   # ...
-
+count=3
   provisioner "local-exec" {
-    command = "kubectl config set-credentials system:node:${instance} --client-certificate=${instance}.pem --client-key=${instance}-key.pem --embed-certs=true  --kubeconfig=${instance}.kubeconfig
+    command = "kubectl config set-credentials system:node:worker-${count.index} --client-certificate=worker-${count.index}.pem --client-key=worker-${count.index}-key.pem --embed-certs=true  --kubeconfig=../../KubernetesConfig/worker-${count.index}.kubeconfig
 "
   }
   
  
 }
 
-resource "null_resource" "Generate-CA" {
+resource "null_resource" "Set-Context" {
   # ...
-
+count=3
   provisioner "local-exec" {
-    command = " kubectl config set-context default --cluster=kubernetes-the-hard-way  --user=system:node:${instance}  --kubeconfig=${instance}.kubeconfig"
+    command = " kubectl config set-context default --cluster=kubernetes-the-hard-way  --user=system:node:worker-${count.index}  --kubeconfig=../../KubernetesConfig/worker-${count.index}.kubeconfig"
   }
   
  
 }
 
-resource "null_resource" "Generate-CA" {
+resource "null_resource" "Set-User-Context" {
   # ...
-
+count=3
   provisioner "local-exec" {
-    command = " kubectl config use-context default --kubeconfig=${instance}.kubeconfig"  }
+    command = " kubectl config use-context default --kubeconfig=../../KubernetesConfig/worker-${count.index}.kubeconfig"  }
   
 }
 
