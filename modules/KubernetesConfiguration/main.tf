c809a7a6034ee6205c288f376ba4e11e32e60f8a
amit <amit.roy2007@gmail.com>
2020-04-21 23:40:08 +0200

    Update main.tf

--- modules/KubernetesConfiguration/main.tf
+++ modules/KubernetesConfiguration/main.tf
@@ -32,17 +32,45 @@ command = " kubectl config set-context default --cluster=kubernetes-the-hard-way
  
 }
 
-resource "null_resource" "Set-User-Context" {
+
+###Kube Scheduler Config
+
+resource "null_resource" "Set-Scheduler-Cluster" {
   # ...
-  depends_on       = ["null_resource.Configure-Cluster","null_resource.Set-Credentials-Cluster","null_resource.Set-Context"]
-count=3
-  provisioner "local-exec" {
-    command = " kubectl config use-context default --kubeconfig=../../KubernetesConfig/worker-${count.index}.kubeconfig" 
+provisioner "local-exec" {
+command = "kubectl config set-cluster kubernetes-the-hard-way --certificate-authority=../../Certificate/ca.pem --embed-certs=true --server=https://127.0.0.1:6443 --kubeconfig=../../KubernetesConfig/kube-scheduler.kubeconfig" 
     }
   
 }
 
+resource "null_resource" "Set-Scheduler-Credential" {
+  # ...
+provisioner "local-exec" {
+command = "kubectl config set-credentials system:kube-scheduler --client-certificate=../../Certificate/kube-scheduler.pem --client-key=kube-../../Certificate/scheduler-key.pem --embed-certs=true --kubeconfig=../../KubernetesConfig/kube-scheduler.kubeconfig
+ "    }
+  
+}
 
+
+resource "null_resource" "Set-kubeScheduler-Context" {
+  # ...
+provisioner "local-exec" {
+command = "kubectl config set-context default --cluster=kubernetes-the-hard-way --user=system:kube-scheduler --kubeconfig=../../KubernetesConfig/kube-scheduler.kubeconfig
+
+ "    }
+  
+}
+
+
+resource "null_resource" "Set-kubeScheduler-User-Context" {
+  # ...
+  #depends_on       = ["null_resource.Configure-Cluster","null_resource.Set-Credentials-Cluster","null_resource.Set-Context"]
+  provisioner "local-exec" {
+    command = "kubectl config use-context default --kubeconfig=../../KubernetesConfig/kube-scheduler.kubeconfig" 
+    }
+  
+}
+  
 
 
  
