1f51ab83e28f4cf3f4d714f9bf6a47853b5e048e
amit <amit.roy2007@gmail.com>
2020-04-22 23:51:56 +0200

    Update main.tf

--- modules/KubernetesConfiguration/main.tf
+++ modules/KubernetesConfiguration/main.tf
@@ -114,6 +114,7 @@ command ="kubectl config set-cluster kubernetes-the-hard-way --certificate-autho
 
 resource "null_resource" "Set-Scheduler-Credential" {
   # ...
+  depends_on       = ["null_resource.Set-Schedule-Cluster"]
 provisioner "local-exec" {
 command ="kubectl config set-credentials system:kube-scheduler --client-certificate=../../Certificate/kube-scheduler.pem --client-key=kube-../../Certificate/kube-scheduler-key.pem --embed-certs=true --kubeconfig=../../KubernetesConfig/kube-scheduler.kubeconfig"
   }
