6c962ea1fa26f7db377fe6ea6ddd0a84b79543b3
amit <amit.roy2007@gmail.com>
2020-04-23 03:33:45 +0200

    Update main.tf

--- modules/KubernetesConfiguration/main.tf
+++ modules/KubernetesConfiguration/main.tf
@@ -1,31 +1,18 @@
 
 ########-----------------------------The kubelet Kubernetes Configuration File-------------------##
-resource "null_resource" "Configure-Cluster" {
+resource "null_resource" "The kubelet Kubernetes Configuration File" {
   # ...
   count =3
   provisioner "local-exec" {
   command ="kubectl config set-cluster kubernetes-the-hard-way --certificate-authority=../../Certificate/ca.pem --embed-certs=true  --server=https://${var.KUBERNETES_PUBLIC_ADDRESS}:6443 --kubeconfig=../../KubernetesConfig/worker-${count.index}.kubeconfig"
   }
   
- 
-}
-
-resource "null_resource" "Set-Credentials-Cluster" {
-  # ...
-depends_on       = ["null_resource.Configure-Cluster"]
-  count=3
   
 provisioner "local-exec" {
 command ="kubectl config set-credentials system:node:worker-${count.index} --client-certificate=../../Certificate/worker-${count.index}.pem --client-key=../../Certificate/worker-${count.index}-key.pem --embed-certs=true  --kubeconfig=../../KubernetesConfig/worker-${count.index}.kubeconfig"
   }
   
- 
-}
 
-resource "null_resource" "Set-Context" {
-  # ...
-  depends_on       = ["null_resource.Set-Credentials-Cluster"]
-count=3
 provisioner "local-exec" {
 command ="kubectl config set-context default --cluster=kubernetes-the-hard-way  --user=system:node:worker-${count.index}  --kubeconfig=../../KubernetesConfig/worker-${count.index}.kubeconfig"
   }
